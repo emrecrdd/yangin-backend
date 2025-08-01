9660efb2ae35dbe4ee683978a35b0406d5f22905
Emre <121616048+emrecrdd@users.noreply.github.com>
2025-08-01 16:36:04 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,11 +5,10 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  protocol: "postgres",
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false
+      rejectUnauthorized: false // Production'da true yapılmalı, test için false
     }
   },
   logging: false,
