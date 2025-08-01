7aca078acb95c70a381f4f96ec1b1c8fde5745c0
Emre <121616048+emrecrdd@users.noreply.github.com>
2025-08-01 14:37:21 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,22 +1,19 @@
-// /config/database.js
 const { Sequelize } = require("sequelize");
 const dotenv = require("dotenv");
-
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,     // Veritabanı adı
-  process.env.DB_USER,     // Kullanıcı
-  process.env.DB_PASS, // Şifre
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    logging: false, // Konsolda SQL loglarını kapatmak için
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+  logging: false,
+});
 
-// Bağlantıyı test et
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
