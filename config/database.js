cf1177be207cd8de06e135fc68d71e7959e76be1
Emre <121616048+emrecrdd@users.noreply.github.com>
2025-08-01 14:52:22 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
 const { Sequelize } = require("sequelize");
 const dotenv = require("dotenv");
+
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
-  protocol: "postgres",
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false,
+      rejectUnauthorized: false, // Neon self-signed SSL kullanıyor olabilir
     },
   },
   logging: false,
