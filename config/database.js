012125425a1dd98678f3b0f6c5b4e274330ae4f4
Emre <121616048+emrecrdd@users.noreply.github.com>
2025-08-01 17:03:36 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,21 +1,32 @@
+const { Sequelize } = require("sequelize");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
 const isProduction = process.env.NODE_ENV === "production";
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "postgres",
-    logging: false,
-    dialectOptions: isProduction
-      ? {
-          ssl: {
-            require: true,
-            rejectUnauthorized: false,
-          },
-        }
-      : undefined,
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  protocol: "postgres",
+  logging: false,
+  dialectOptions: isProduction
+    ? {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      }
+    : {},
+});
+
+const connectDB = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("🟢 PostgreSQL bağlantısı başarılı.");
+  } catch (error) {
+    console.error("🔴 Veritabanı bağlantı hatası:", error.message);
+    process.exit(1);
   }
-);
+};
+
+module.exports = { sequelize, connectDB };
