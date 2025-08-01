d9efc525c7e6fb7d8cf0de285d95937690b69d71
Emre <121616048+emrecrdd@users.noreply.github.com>
2025-08-01 16:18:36 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,15 +5,17 @@ dotenv.config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  protocol: "postgres",
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false,
-    },
+      rejectUnauthorized: false
+    }
   },
   logging: false,
 });
 
+// Bağlantıyı test et
 const connectDB = async () => {
   try {
     await sequelize.authenticate();
